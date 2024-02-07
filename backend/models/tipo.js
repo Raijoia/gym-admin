5358d4353b499e04d9f0a0d331767f46d46ea27a
Raí Joia <megac154@gmail.com>
2024-02-07 08:35:28 -0300

    feat: association entidade tipo

--- backend/models/tipo.js
+++ backend/models/tipo.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class tipo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Tipo.hasMany(models.Personal, {
+        foreignKey: 'tipo_id'
+      })
+      Tipo.hasMany(models.Cliente, {
+        foreignKey: 'tipo_id'
+      })
     }
   }
   tipo.init({
