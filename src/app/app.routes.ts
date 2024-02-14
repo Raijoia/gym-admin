9492817c379b105d2cbb821abf30b1a94431b0e4
Raí Joia <megac154@gmail.com>
2024-02-14 15:49:15 -0300

    feat: pagina inicio ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { CadastrarPersonalComponent } from './components/home/cadastrar-personal/cadastrar-personal.component';
+import { InicioComponent } from './components/home/inicio/inicio.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+    pathMatch: 'full',
+  },
   {
     path: 'cadastrarPersonal',
     component: CadastrarPersonalComponent,
