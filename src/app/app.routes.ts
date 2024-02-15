7dd3f602497701a3c44e8af9143e668976ccd171
Raí Joia <megac154@gmail.com>
2024-02-15 15:37:09 -0300

    feat: tela de login ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CadastrarPersonalComponent } from './components/home/cadastrar-personal/cadastrar-personal.component';
 import { InicioComponent } from './components/home/inicio/inicio.component';
+import { LoginComponent } from './components/home/login/login.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'cadastrarPersonal',
     component: CadastrarPersonalComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
